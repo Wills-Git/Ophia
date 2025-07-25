3057eb201f3cc4741392ce10b8a8f6ca65475bed
Wills-Git <williaminterlock@gmail.com>
2025-07-25 13:12:15 -0500

    deployment test - changed runtime stack to match project

--- Program.cs
+++ Program.cs
@@ -17,6 +17,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.MapGet("/", () => "green");
+app.MapGet("/", () => "green dot");
 app.MapControllers();  
 app.Run();
